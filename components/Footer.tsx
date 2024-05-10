a62a367ecba4ed8027c7b70bc0dbe6c12dc5c434
aguspranyoto <agusprnyt@gmail.com>
2024-05-10 21:55:20 +0700

    update

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,21 +1,7 @@
 import React from "react";
 
 const Footer = () => {
-  return (
-    <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
-      <p className="text-xs text-gray-500 dark:text-gray-400">
-        © 2024 Agus Pranyoto. All rights reserved.
-      </p>
-      <nav className="sm:ml-auto flex gap-4 sm:gap-6">
-        <a className="text-xs hover:underline underline-offset-4" href="#">
-          About
-        </a>
-        <a className="text-xs hover:underline underline-offset-4" href="#">
-          Contact
-        </a>
-      </nav>
-    </footer>
-  );
+  return <footer>footer</footer>;
 };
 
 export default Footer;
